38e663da933bb464cdae6a19b3f161c8659e5e57
unknown <jdaguidandelena@gmail.com>
2025-12-03 19:45:08 -0500

    coming soon

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+import ComingSoon from "../src/components/comingSoon";
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+
+    <ComingSoon/>
+      
+    </>
+  )
+}
+
+export default App
